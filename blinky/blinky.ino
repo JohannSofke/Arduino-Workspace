b605f140295b81e4d4b8a7a56e39908bdfd6ac20
JohannSofke <79168826+JohannSofke@users.noreply.github.com>
2025-12-16 19:44:10 +0000

    Implement USB MIDI functionality with message handling and device connection notifications

--- blinky/blinky.ino
+++ blinky/blinky.ino
@@ -1,11 +1,28 @@
 #include <Adafruit_NeoPixel.h>
 #include <AMY-Arduino.h>
-
+#include "UsbMidi.h"
 
 #define PIN 48
 #define NUMPIXELS 1
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+UsbMidi usbMidi;
+
+void onMidiMessage(const uint8_t (&data)[4]) {
+    Serial.println("MIDI Message Received:");
+
+    pixels.setPixelColor(0, pixels.Color(0, 255, 0));
+    pixels.show();
+
+    delay(500);
+
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+    pixels.show();          
+}
+
+void onDeviceConnect() {
+    Serial.println("MIDI Device Connected");
+}
 
 void setup() {
     Serial.begin(115200);
@@ -15,18 +32,14 @@ void setup() {
 
     amy_config_t amy_config = amy_default_config();
     amy_start(amy_config);
+    amy_live_start();
+
+    usbMidi.onMidiMessage(onMidiMessage);
+    usbMidi.onDeviceConnected(onDeviceConnect);
+    usbMidi.begin();
 }
 
 void loop() {
-    Serial.println("Hello World");
-
-    pixels.setPixelColor(0, pixels.Color(255, 0, 0));
-    pixels.show();
-    
-    delay(500);
-
-    pixels.setPixelColor(0, pixels.Color(0, 0, 255));
-    pixels.show();
-    
-    delay(500);
+    usbMidi.update();
+    amy_update();
 }
